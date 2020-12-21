be6f9d1266935f489f904ae8dc9f72fa25acfcb4
elavr <ernestslavrinovics@gmail.com>
2020-12-21 20:01:47 +0100

    Code cleanup for main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,48 +15,42 @@ int main(int argc, char** argv )
         return 0;
     }
 
-    int threshold = 100;
-
-		// Placeholders for frames
-    cv::Mat frame;
-    cv::Mat gray;
+    auto threshold = std::make_shared<float>(0.25f);
+    
+	// Placeholders for frames
+    cv::Mat input;
+    cv::Mat prev_input;
 
     std::cout << "Stream started" << std::endl;
     while (true) {
-        vidCap >> frame;
-        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        prev_input = input;
+        vidCap >> input;
+        cv::cvtColor(input, input, cv::COLOR_BGR2GRAY); // conv color to gray
 
-        for (size_t i = 1; i < gray.rows - 1; i++) {
-            for (size_t j = 1; j < gray.cols; j ++) {
-                /* Thresholding */
-                auto pix = gray.at<uchar>(i, j);
+        for (size_t i = 1; i < input.rows; i++) {
+            for (size_t j = 1; j < input.cols; j ++) {
+                
+                auto pix = Utilities::GetPixelValue(input, i, j);
 
-                if (pix < threshold) {
-                    gray.at<uchar>(i, j) = 0;
+                if ( pix < *threshold.get() ) {
+                    Utilities::SetPixelValue(input, i, j, 0.f);
                 }
                 else {
-                    gray.at<uchar>(i, j) = 255;
+                    Utilities::SetPixelValue(input, i, j, 1.f);
                 }
             }
         }
 
-        if( gray.empty() ) break; // end of video stream
+        if( input.empty() ) break; // end of video stream
         cv::namedWindow("Gray", cv::WINDOW_AUTOSIZE);
-        cv::imshow("Gray", gray);
+        cv::imshow("Gray", input);
 
-        if( cv::waitKey(10) == 120 ) { // Button X pressed
-            threshold++; // stop capturing by pressing ESC
-            std::cout << "Variable " << threshold << std::endl;
-        }
-        if( cv::waitKey(10) == 122 ) { // Button Z pressed
-            threshold--; // stop capturing by pressing ESC
-            std::cout << "Variable " << threshold << std::endl;
-        }
+        if( cv::waitKey(10) == 120 ) Utilities::IncreaseInt(threshold);
+        if( cv::waitKey(10) == 122 ) Utilities::DecreaseInt(threshold);
         if( cv::waitKey(10) == 27 ) break; // stop capturing by pressing ESC
     }
-
+    
     vidCap.release();
     cv::destroyAllWindows();
-
     return 0;
 }
