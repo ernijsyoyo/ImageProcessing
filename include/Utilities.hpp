1381c2763f70903dcb817aff95d260477e30610d
elavr <ernestslavrinovics@gmail.com>
2020-12-21 20:01:14 +0100

    Add helper functions for easy get/set of pixels and merging two frames

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -1,3 +1,5 @@
+#include <memory>
+#include <opencv2/opencv.hpp>
 #pragma once
 
 class Utilities {
@@ -7,5 +9,13 @@ public:
     Utilities();
     ~Utilities();
     
-    void HelloWorld();
+    static void HelloWorld();
+    /// Return the 
+    static cv::Mat AddTwoFrames(cv::Mat frame1, cv::Mat frame2);
+    // Get int value of pixel at position and convert to float
+    static float GetPixelValue(cv::Mat frame, int x, int y);
+    
+    static void SetPixelValue(cv::Mat frame, int x, int y, float value);
+    static void IncreaseInt(std::shared_ptr<float> input);
+    static void DecreaseInt(std::shared_ptr<float> input);
 };
--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -12,3 +12,32 @@ Utilities::~Utilities(){
 void Utilities::HelloWorld(){
     std::cout << " Hello World " << std::endl;
 }
+
+cv::Mat Utilities::AddTwoFrames(cv::Mat frame1, cv::Mat frame2) {
+    cv::Mat output;
+    cv::addWeighted(frame1, 1.0, frame2, 1.0, 0.0, output);
+    return output;
+}
+
+float Utilities::GetPixelValue(cv::Mat frame, int x, int y) {
+    auto output = static_cast<float>( (int)frame.at<uchar>(x, y) );
+    return output / 255.f;
+}
+
+void Utilities::SetPixelValue(cv::Mat frame, int x, int y, float value) {
+    auto setTo = value * 255.f;
+    auto intValue = static_cast<size_t>(setTo);
+    frame.at<uchar>(x, y) = intValue;
+}
+
+void Utilities::IncreaseInt(std::shared_ptr<float> input){
+    std::cout << "Increasing input " << *input.get() << std::endl;
+    *input += 0.01f;
+    std::cout << "Increased input " << *input.get() << std::endl;
+}
+
+void Utilities::DecreaseInt(std::shared_ptr<float> input){
+    std::cout << "Decreasing input " << *input.get() << std::endl;
+    *input -= 0.01f;
+    std::cout << "Decrease input " << *input.get() << std::endl;
+}
