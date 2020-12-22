cd86f5f6848aa99ba2fe1786e97e29bd37539b89
elavr <ernestslavrinovics@gmail.com>
2020-12-22 16:37:50 +0100

    Cleanup main

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <Utilities.hpp>
+#include <Algorithms.hpp>
 
 int main(int argc, char** argv )
 {
@@ -15,39 +16,40 @@ int main(int argc, char** argv )
         return 0;
     }
 
-    auto threshold = std::make_shared<float>(0.25f);
-    
 	// Placeholders for frames
     cv::Mat input;
     cv::Mat prev_input;
+    cv::namedWindow("Gray", cv::WINDOW_AUTOSIZE);
 
-    std::cout << "Stream started" << std::endl;
+    // Control buttons
+    auto userInput = std::make_shared<float>(0.25f);
+    auto algorithmSwitch = std::make_shared<int>(1);
+
+    // Algorithms
+    auto thresholdStrategy = new ThresholdStrategy;
+    auto motionStrategy = new MotionStrategy;
+    Context *context = new Context(motionStrategy);
+    
+    std::cout << "Stream starting" << std::endl;
     while (true) {
+        // Convert webcam to grayscale
         prev_input = input;
         vidCap >> input;
         cv::cvtColor(input, input, cv::COLOR_BGR2GRAY); // conv color to gray
-
-        for (size_t i = 1; i < input.rows; i++) {
-            for (size_t j = 1; j < input.cols; j ++) {
-                
-                auto pix = Utilities::GetPixelValue(input, i, j);
-
-                if ( pix < *threshold.get() ) {
-                    Utilities::SetPixelValue(input, i, j, 0.f);
-                }
-                else {
-                    Utilities::SetPixelValue(input, i, j, 1.f);
-                }
-            }
-        }
-
+        
+        // Process strategy and display result
+        context->ProcessStrategy(input, userInput);
         if( input.empty() ) break; // end of video stream
-        cv::namedWindow("Gray", cv::WINDOW_AUTOSIZE);
         cv::imshow("Gray", input);
-
-        if( cv::waitKey(10) == 120 ) Utilities::IncreaseInt(threshold);
-        if( cv::waitKey(10) == 122 ) Utilities::DecreaseInt(threshold);
-        if( cv::waitKey(10) == 27 ) break; // stop capturing by pressing ESC
+        
+        
+        // Algorithm control and loop break
+        if (cv::waitKey(5) == 113) break; // stop capturing by pressing ESC
+        if (cv::waitKey(5) == 49) context->set_strategy(thresholdStrategy); // key 1
+        if (cv::waitKey(5) == 50) context->set_strategy(motionStrategy); // key 2
+        if (cv::waitKey(5) == 51) *algorithmSwitch.get() = 3; // key 3
+        if (cv::waitKey(5) == 120) Utilities::IncreaseFloat(userInput); // key z
+        if (cv::waitKey(5) == 122) Utilities::DecreaseFloat(userInput); // key x
     }
     
     vidCap.release();
