5f38ebc277b42329d269107ddafbc65f8789fa51
elavr <ernestslavrinovics@gmail.com>
2020-12-22 16:37:42 +0100

    Add algorithms implemented as strategy pattern

---
+++ include/Algorithms.hpp
@@ -0,0 +1,151 @@
+#include <opencv2/opencv.hpp>
+#include <Utilities.hpp>
+#pragma once
+
+#define GETPIX
+
+/**
+ * The Strategy interface declares operations common to all supported versions
+ * of some algorithm.
+ *
+ * The Context uses this interface to call the algorithm defined by Concrete
+ * Strategies.
+ */
+
+class AlgorithmStrategy {
+public:
+    virtual ~AlgorithmStrategy() {}
+    virtual cv::Mat Process(cv::Mat frame, std::shared_ptr<float> variable = nullptr) const = 0;
+    std::string AlgorithmName; 
+};
+
+/**
+ * The Context defines the interface of interest to clients.
+ */
+
+class Context
+{
+    /**
+     * @var Strategy The Context maintains a reference to one of the Strategy
+     * objects. The Context does not know the concrete class of a strategy. It
+     * should work with all strategies via the Strategy interface.
+     */
+private:
+    AlgorithmStrategy *strategy_;
+    std::shared_ptr<float> m_Variable;
+
+    /**std::vector<std::string>{"a", "e", "c", "b", "d"}
+     * Usually, the Context accepts a strategy through the constructor, but also
+     * provides a setter to change it at runtime.
+     */
+public:
+    Context(AlgorithmStrategy *algStrat = nullptr) {
+        strategy_ = algStrat;
+    }
+    ~Context() {
+        delete this->strategy_;
+    }
+
+    /**
+     * Usually, the Context allows replacing a Strategy object at runtime.
+     */
+    void set_strategy(AlgorithmStrategy *strategy) {
+        strategy_ = strategy;
+        std::cout << "Algorithm set to: " << strategy->AlgorithmName << std::endl;
+    }
+
+    /**
+     * The Context delegates some work to the Strategy object instead of
+     * implementing +multiple versions of the algorithm on its own.
+     */
+    void ProcessStrategy(cv::Mat frame, std::shared_ptr<float> userInput = nullptr) const {
+        cv::Mat result = this->strategy_->Process(frame, userInput);
+    }
+};
+
+/**
+ * Concrete Strategies implement the algorithm while following the base Strategy
+ * interface. The interface makes them interchangeable in the Context.
+ */
+class ThresholdStrategy : public AlgorithmStrategy {
+public:
+
+    ThresholdStrategy(){
+        AlgorithmName = "Thresholding";
+    }
+
+    cv::Mat Process(cv::Mat frame, std::shared_ptr<float> variable=nullptr) const override {
+        assert(variable != nullptr);
+
+        auto rows = frame.rows;
+        auto columns = frame.cols;
+        float threshold = static_cast<float>(*variable.get());
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < columns; j++) {
+
+                if(Utilities::GetPixelValue(frame, i, j) < threshold ){
+                    Utilities::SetPixelValue(frame, i, j, 0.0f);
+                }
+                else {
+                    Utilities::SetPixelValue(frame, i, j, 1.0f);
+                }
+            }
+        }
+        return frame;
+    }
+};
+
+/**
+ * Concrete Strategies implement the algorithm while following the base Strategy
+ * interface. The interface makes them interchangeable in the Context.
+ */
+class MotionStrategy : public AlgorithmStrategy {
+private:
+    cv::Mat *prev_Frame = new cv::Mat(640, 480, CV_8UC1);
+
+public:
+    MotionStrategy(){
+        AlgorithmName = "Motion Algorithm";
+    }
+    cv::Mat Process(cv::Mat frame, std::shared_ptr<float> variable=nullptr) const override {
+        assert(variable != nullptr);
+        if (prev_Frame->empty()) {
+            frame.copyTo(*prev_Frame);
+            return frame;
+        }
+
+        auto rows = frame.rows;
+        auto columns = frame.cols;
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < columns; j++) {
+                auto diff = fabs(Utilities::GetPixelValue(frame, i, j) - Utilities::GetPixelValue(*prev_Frame, i, j));
+                Utilities::SetPixelValue(frame, i, j, diff);
+            }
+        }
+        frame.copyTo(*prev_Frame);
+        return frame;
+    }
+};
+
+/**
+ * Concrete Strategies implement the algorithm while following the base Strategy
+ * interface. The interface makes them interchangeable in the Context.
+ */
+class ExampleStrategy : public AlgorithmStrategy {
+public:
+    ExampleStrategy(){
+        AlgorithmName = "Example Algorithm";
+    }
+    cv::Mat Process(cv::Mat frame, std::shared_ptr<float> variable=nullptr) const override {
+        assert(variable != nullptr);
+
+        auto rows = frame.rows;
+        auto columns = frame.cols;
+        float threshold = static_cast<float>(*variable.get());
+
+        /* Code */
+        return frame;
+    }
+};
